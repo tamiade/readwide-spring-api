73b806408b84605f0e08ec648aeaa753ba86dc4a
Tami <tamiadetayo@gmail.com>
2023-01-26 03:11:44 -0800

    Initial setup

---
+++ src/main/java/com/capstone/readWide/ReadWideApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.readWide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadWideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadWideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/readWide/ReadWideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.readWide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadWideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
