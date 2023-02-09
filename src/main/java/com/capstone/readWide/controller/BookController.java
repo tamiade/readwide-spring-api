4d579348318761e107659d4ff0a67e110f4af94c
Tami <tamiadetayo@gmail.com>
2023-02-08 17:23:31 -0800

    Cleaned up code

--- src/main/java/com/capstone/readWide/controller/BookController.java
+++ src/main/java/com/capstone/readWide/controller/BookController.java
@@ -3,7 +3,6 @@ package com.capstone.readWide.controller;
 import java.lang.Iterable;
 import java.util.List;
 import java.util.ArrayList;
-import io.github.cdimascio.dotenv.Dotenv;
 
 import com.capstone.readWide.model.Book;
 import com.capstone.readWide.repository.BookRepository;
@@ -19,23 +18,12 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    Dotenv dotenv = Dotenv.load();
-    private String apiKey = dotenv.get("GOOGLE_API_KEY");
-    // private WebClient webClient = WebClient.create("https://www.googleapis.com");
 
     private final BookRepository bookRepository;
 
     public BookController(final BookRepository bookRepository) {
     this.bookRepository = bookRepository;
     }
-
-    // public Flux<Book> getGoogleBooks(String searchTerm) {
-    //     return this.webClient.get().uri(uriBuilder -> uriBuilder
-    //             .path("/books/v1/volumes")
-    //             .queryParam("q", searchTerm)
-    //             .queryParam("key", apiKey)
-    //             .build()).retrieve().bodyToFlux(Book.class);
-    // }
 
     @GetMapping
     public Iterable<Book> getAllBooks() {
@@ -54,17 +42,12 @@ public class BookController {
         }
     }
 
-    // @PostMapping
-    // public ArrayList<Book> addBook(@RequestParam(name="title") String searchTerm) {
-    //     ArrayList<Book> newBook = getGoogleBooks(searchTerm);
-    //     return newBook;
-
-    // @PostMapping
-    // // @ResponseStatus(code = HttpStatus.CREATED, reason = "Book was successfully added!")
-    // public Book addBook(@RequestBody Book book) {
-    //     // throw new ResponseStatusException(HttpStatus.CONFLICT,
-    //     //         "This Book already exists on ReadWide! You can add a reflection instead");
-    //     Book newBook = this.bookRepository.save(book);
-    //     return newBook;
-    // }
+    @PostMapping
+    // @ResponseStatus(code = HttpStatus.CREATED, reason = "Book was successfully added!")
+    public Book addBook(@RequestBody Book book) {
+        // throw new ResponseStatusException(HttpStatus.CONFLICT,
+        //         "This Book already exists on ReadWide! You can add a reflection instead");
+        Book newBook = this.bookRepository.save(book);
+        return newBook;
+    }
 }
