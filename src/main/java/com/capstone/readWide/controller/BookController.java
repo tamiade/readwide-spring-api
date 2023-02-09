8c92a27e1e4512497763052f2fe5cc51b1895941
Tami <tamiadetayo@gmail.com>
2023-02-08 20:08:49 -0800

    All routes tested and working as intended

--- src/main/java/com/capstone/readWide/controller/BookController.java
+++ src/main/java/com/capstone/readWide/controller/BookController.java
@@ -3,6 +3,7 @@ package com.capstone.readWide.controller;
 import java.lang.Iterable;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import com.capstone.readWide.model.Book;
 import com.capstone.readWide.repository.BookRepository;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.util.ObjectUtils;
 
 @RestController
 @RequestMapping("/books")
@@ -31,22 +34,30 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    public List<Book> searchBooks(@RequestParam(name="title", required=false) String title, @RequestParam(name="author", required=false) String author) {
-        // throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sorry, the Book you entered does not exist on ReadWide!");
-        if (title != null) {
+    public Optional<Book> searchBooks(@RequestParam(name="title", required=false) String title, @RequestParam(name="author", required=false) String author) {
+        if (title != null && author != null) {
+            return this.bookRepository.findByTitleAndAuthor(title, author);
+        } else if (title != null) {
             return this.bookRepository.findByTitle(title);
         } else if (author != null) {
             return this.bookRepository.findByAuthor(author);
         } else {
-            return new ArrayList<>();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sorry, you must enter a valid title or author, or the Book you entered does not exist on ReadWide!");
         }
     }
 
     @PostMapping
-    // @ResponseStatus(code = HttpStatus.CREATED, reason = "Book was successfully added!")
+    @ResponseStatus(code = HttpStatus.CREATED, reason = "Book was successfully added!")
     public Book addBook(@RequestBody Book book) {
-        // throw new ResponseStatusException(HttpStatus.CONFLICT,
-        //         "This Book already exists on ReadWide! You can add a reflection instead");
+        if (ObjectUtils.isEmpty(book.getTitle())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You must input a title to add a book to ReadWide!");
+        }
+        Optional<Book> existingBook = bookRepository
+                .findByTitleAndAuthor(book.getTitle(), book.getAuthor());
+        if (existingBook.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, 
+                    "This Book already exists on ReadWide! You can add a reflection instead!");
+        }
         Book newBook = this.bookRepository.save(book);
         return newBook;
     }
--- src/main/java/com/capstone/readWide/repository/BookRepository.java
+++ src/main/java/com/capstone/readWide/repository/BookRepository.java
@@ -1,13 +1,15 @@
 package com.capstone.readWide.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import com.capstone.readWide.model.Book;
 
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
-    List<Book> findByTitle(String title);
-    List<Book> findByAuthor(String author);
+    Optional<Book> findByTitle(String title);
+    Optional<Book> findByAuthor(String author);
+    Optional<Book> findByTitleAndAuthor(String title, String author);
 }
 
